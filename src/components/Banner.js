2059eff59b04d9c11912ec0d4ce8b1d983adbd08
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-06-26 18:07:34 +0530

    The wonderful looking banner using react-responsive-carousel library component: this is amazing

---
+++ src/components/Banner.js
@@ -0,0 +1,37 @@
+
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+function Banner() {
+    return (
+        <div className="relative" >
+            <Carousel
+                autoPlay
+                infiniteLoop
+                showStatus={false}
+                showIndicators={false}
+                showThumbs={false}
+                interval={5000}
+            >
+                <div>
+                    <img loading="lazy" src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Home/AmazonTV/Ravina/Dice-Media--MBBS-Web-series---Graphics_1500x600._CB665320173_.jpg" alt="" />
+                </div>
+                <div>
+                    <img loading="lazy" src="https://images-eu.ssl-images-amazon.com/images/G/31/Gateway/Zeitgeist/Test/IQOO__ad_test_1500-x-600_updated._CB665297553_.jpg" alt="" />
+                </div>
+                <div>
+                    <img loading="lazy" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/CEPC/Clearance/Jun21/D23947948_IN_CEPC_Clearance_store_May21_rush_3000x12000.5x._CB665291878_.jpg" alt="" />
+                </div>
+                <div>
+                    <img loading="lazy" src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Avatar/HeroPC_1500x600_SVA._CB667240774_.jpg" alt="" />
+                </div>
+                <div>
+                    <img loading="lazy" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Audio/unrec1499/Under1499_Gw_1500x600._CB661592357_.jpg" alt="" />
+                </div>
+
+            </Carousel>
+        </div>
+    )
+}
+
+export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from './../components/Header';
+import Banner from './../components/Banner';
 
 export default function Home() {
   return (
@@ -9,6 +10,14 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* banner */}
+        <Banner />
+
+        {/* product feeds */}
+
+      </main>
      
     </div>
   );
